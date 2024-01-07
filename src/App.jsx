104dff36f687e94306f88bf1d1a8545d8aaf6f65
Deven Umrania <umraniadeven10@gmail.com>
2024-01-07 20:04:04 +0530

    update: 1) added dependencies 2) created folder basic structure 3) added routes & navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,27 @@
+import { Routes, Route } from "react-router-dom";
+
+// routes
+import Products from "./Pages/Products/Products";
+import Cart from "./Pages/Cart/Cart";
+
+// css files
+import "react-loading-skeleton/dist/skeleton.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 
+// templates
+import Navbar from "./Templates/Navbar/Navbar";
+
 function App() {
-  return <></>;
+  return (
+    <div>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Products />} />
+        <Route path="/cart" element={<Cart />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/Templates/Navbar/Navbar.jsx
@@ -0,0 +1,41 @@
+import { NavLink, useNavigate } from "react-router-dom";
+
+import styles from "./Navbar.module.css";
+
+const Navbar = () => {
+  const navigate = useNavigate();
+  return (
+    <div
+      className={`w-100 d-flex justify-content-between align-items-center gap-4 ${styles.navbar}`}
+    >
+      {/* logo */}
+      <img
+        src="../../src/assets/cropped-Dil-Foods-new-logo-180x180.png"
+        alt="logo"
+        loading="lazy"
+        onClick={() => navigate("/")}
+      />
+
+      {/* page links  */}
+      <nav className={`d-flex gap-5 align-items-center nav`}>
+        <NavLink to="/">Products</NavLink>
+        <NavLink to="/cart">Cart</NavLink>
+      </nav>
+
+      {/* deven socials */}
+      <nav className="d-flex align-items-center gap-4 nav">
+        <NavLink to="https://deven-portfolio.netlify.app/" target="_blank">
+          Portfolio
+        </NavLink>
+        <NavLink to="https://www.linkedin.com/in/umraniadeven/" target="_blank">
+          LinkedIn
+        </NavLink>
+        <NavLink to="https://github.com/deven10" target="_blank">
+          Github
+        </NavLink>
+      </nav>
+    </div>
+  );
+};
+
+export default Navbar;
