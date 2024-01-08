b03790867a76df265028c9839c56b9e2dd86733e
Deven Umrania <umraniadeven10@gmail.com>
2024-01-09 00:42:10 +0530

    update: 1) added manifest file for PWA 2) added dummy data

---
+++ src/data/data.js
@@ -0,0 +1,133 @@
+export const foodList = [
+  {
+    id: 1,
+    title: "Creamy Tomato Soup",
+    genre: ["Starter", "Baking"],
+    rating: 7,
+    price: 300,
+    summary:
+      "Creamy tomato soup blends ripe tomatoes with coconut milk or cashew cream, delivering a lusciously smooth and comforting vegan twist on the timeless classic. Garnish with fresh herbs for a flavorful bowl that warms both body and soul.",
+    imageURL:
+      "https://www.themealdb.com/images/media/meals/stpuws1511191310.jpg",
+    videoUrl: "https://youtu.be/lBhwjjUiFk4",
+  },
+  {
+    id: 2,
+    title: "Madeira Cake",
+    genre: ["Cake", "Light", "Baking", "Desert"],
+    rating: 8,
+    price: 400,
+    summary:
+      "Madeira cake, a British classic, boasts a rich and buttery flavor with a hint of lemon zest. Its dense yet moist texture makes it a delightful tea-time treat, perfect for any occasion.",
+    imageURL:
+      "https://www.themealdb.com/images/media/meals/urtqut1511723591.jpg",
+    videoUrl: "https://youtu.be/-YDh4WEmK_E",
+  },
+  {
+    id: 3,
+    title: "Dundee cake",
+    genre: ["Heavy", "Nutty", "Fruity"],
+    rating: 6,
+    price: 250,
+    summary:
+      "Dundee cake, a traditional Scottish dessert, is a fruitcake distinguished by its generous mix of currants, sultanas, and almonds. The aromatic zest of orange and lemon adds a distinctive flavor, making it a cherished and timeless delight.",
+    imageURL:
+      "https://www.themealdb.com/images/media/meals/wxyvqq1511723401.jpg",
+    videoUrl: "https://youtu.be/4hEXsfpeMQE",
+  },
+  {
+    id: 4,
+    title: "Vegan Chocolate Cake",
+    genre: ["Vegan", "Chocolate", "Cake"],
+    rating: 6,
+    price: 500,
+    summary:
+      "Vegan chocolate cake indulges the sweet tooth without dairy or eggs, featuring a moist, cocoa-infused sponge crowned with dairy-free frosting. A guilt-free, decadent delight for plant-based dessert lovers.",
+    imageURL:
+      "https://www.themealdb.com/images/media/meals/qxutws1486978099.jpg",
+    videoUrl: "https://youtu.be/C3pAgB7pync",
+  },
+  {
+    id: 5,
+    title: "New York cheesecake",
+    genre: ["Desert", "Dairy", "Pudding", "Cake", "Breakfast"],
+    rating: 8,
+    price: 700,
+    summary:
+      "New York cheesecake, a velvety and dense delight, combines cream cheese, sugar, and vanilla atop a graham cracker crust. Baked to perfection, it delivers a classic, rich flavor that defines the iconic American dessert.",
+    imageURL:
+      "https://www.themealdb.com/images/media/meals/swttys1511385853.jpg",
+    videoUrl: "https://youtu.be/tspdJ6hxqnc",
+  },
+  {
+    id: 6,
+    title: "Mushroom & Chestnut Rotolo",
+    genre: ["Vegetarian", "Nutty"],
+    rating: 5,
+    price: 550,
+    summary:
+      "Mushroom and chestnut rotolo is a savory delight, featuring pasta sheets rolled with a filling of earthy mushrooms and chestnuts. Baked to perfection and served with a flavorful sauce, it's a comforting and elegant dish for any occasion.",
+    imageURL:
+      "https://www.themealdb.com/images/media/meals/ssyqwr1511451678.jpg",
+    videoUrl: "https://youtu.be/GNN7_ZSJ5YE",
+  },
+  {
+    id: 7,
+    title: "Ribollita",
+    genre: ["Vegetarian"],
+    rating: 6,
+    price: 450,
+    summary:
+      "Ribollita, a Tuscan soup, is a hearty concoction of vegetables, beans, and bread, simmered and then reheated for a flavorful encore. This rustic dish, literally meaning 'reboiled,' epitomizes Italian comfort food with its robust taste and nourishing simplicity.",
+    imageURL:
+      "https://www.themealdb.com/images/media/meals/xrrwpx1487347049.jpg",
+    videoUrl: "https://youtu.be/BiQUYTBb6eQ",
+  },
+  {
+    id: 8,
+    title: "Sticky Toffee Pudding Ultimate",
+    genre: ["Pudding", "Desert", "Cake", "Dairy"],
+    rating: 7,
+    price: 600,
+    summary:
+      "Ultimate sticky toffee pudding, a British dessert masterpiece, features moist date-infused sponge drenched in rich toffee sauce. Indulge in the decadence of this sweet delight for a heavenly, comforting treat.",
+    imageURL:
+      "https://www.themealdb.com/images/media/meals/xrptpq1483909204.jpg",
+    videoUrl: "https://youtu.be/hKq6RbxJHBo",
+  },
+  {
+    id: 9,
+    title: "Dal fry",
+    genre: ["Curry", "Vegetarian", "Cake"],
+    rating: 5,
+    price: 250,
+    summary:
+      "Dal fry, a popular Indian dish, involves tempering spices in ghee, then simmering lentils (dal) to perfection. The aromatic blend of spices enhances the flavor, creating a wholesome and flavorful lentil curry.",
+    imageURL:
+      "https://www.themealdb.com/images/media/meals/wuxrtu1483564410.jpg",
+    videoUrl: "https://youtu.be/J4D855Q9-jg",
+  },
+  {
+    id: 10,
+    title: "Chelsea Buns",
+    genre: ["Bun", "Baking", "Treat"],
+    rating: 5,
+    price: 300,
+    summary:
+      "Chelsea buns, a British pastry, are sweet, spiced rolls filled with currants or raisins, rolled up, sliced, and baked until golden. Glazed with a sugary syrup, these delightful buns offer a perfect blend of sweetness and warmth.",
+    imageURL:
+      "https://www.themealdb.com/images/media/meals/vqpwrv1511723001.jpg",
+    videoUrl: "https://youtu.be/i_zemP3yBKw",
+  },
+  {
+    id: 11,
+    title: "Breakfast Potatoes",
+    genre: ["Breakfast", "Brunch"],
+    rating: 8,
+    price: 400,
+    summary:
+      "Breakfast potatoes, a savory morning delight, are diced or sliced potatoes seasoned with herbs and spices, then pan-fried or roasted until crispy and golden. This classic side dish adds a hearty and flavorful touch to your morning meal.",
+    imageURL: "https://www.themealdb.com/images/media/meals/1550441882.jpg",
+    videoUrl: "https://youtu.be/BoD0TIO9nE4",
+  },
+];
